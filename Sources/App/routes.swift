bd4322000880e6575ac2eb5bc73a96fd0ce50633
Babur <bduisenov@gmail.com>
2018-02-05 09:12:27 +0100

    first commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Async
+import Routing
+import Vapor
+import WebSocket
+import Crypto
+import HTTP
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of creating a Service and using it.
+    router.get("hash", String.parameter) { req -> String in
+        // Create a BCryptHasher using the Request's Container
+        let hasher = try req.make(BCryptHasher.self)
+
+        // Fetch the String parameter (as described in the route)
+        let string = try req.parameter(String.self)
+
+        // Return the hashed string!
+        return try hasher.make(string)
+    }
+
+    router.websocket("ws") { (req, ws) in
+        ws.onString { websocket, string in
+            websocket.send(string: string)
+        }
+    }
+}
